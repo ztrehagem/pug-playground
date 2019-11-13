dfe5526f9a2107c90ba7f3b78a193696825c77b7
Shigeki Suwa <shigeki.suwa@oro.com>
2019-11-13 20:21:15 +0900

    commit

--- pug/plugins/scoped-html.js
+++ pug/plugins/scoped-html.js
@@ -1,7 +1,43 @@
-const { inspect } = require('util')
+// const { inspect } = require('util')
+const crypt = require('crypto')
+
+const ignoreTags = [
+  'html',
+  'head',
+  'meta',
+  'title',
+  'base',
+  'link',
+  'body',
+  'script',
+]
+
+function canBeScoped (node, options) {
+  return options.src && (
+    (node.type === 'Tag' && !ignoreTags.includes(node.name)) ||
+    (node.type === 'Mixin' && node.call)
+  )
+}
+
+function visitNode (node, options) {
+  if (canBeScoped(node, options)) {
+    const digest = crypt.createHash('md5').update(options.src).digest('hex')
+    node.attrs.push({
+      name: `data-scope-${digest.slice(0, 8)}`,
+      val: '""',
+      mustEscape: false,
+    })
+  }
+  const nodes = (node.block || {}).nodes || []
+  nodes.forEach(node => visitNode(node, options))
+}
 
 function scopedHtml (parsed, options) {
-  console.log('plugin!', inspect(parsed, { depth: null }), options)
+  if (parsed.type === 'Block') {
+    (parsed.nodes || []).forEach(node => visitNode(node, options))
+  } else {
+    visitNode(parsed, options)
+  }
   return parsed
 }
 
